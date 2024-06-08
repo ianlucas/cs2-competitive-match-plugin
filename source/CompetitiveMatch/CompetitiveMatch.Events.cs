f7dbfe564f26807bccdcc8a6fe9a9cd766d64e2a
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-06-08 18:14:01 -0300

    refactor: create partial for `Listeners`

--- source/CompetitiveMatch/CompetitiveMatch.Events.cs
+++ source/CompetitiveMatch/CompetitiveMatch.Events.cs
@@ -7,7 +7,6 @@ using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Commands;
 using CounterStrikeSharp.API.Modules.Cvars;
-using CounterStrikeSharp.API.Modules.Events;
 using CounterStrikeSharp.API.Modules.Timers;
 using CounterStrikeSharp.API.Modules.Utils;
 using Microsoft.Extensions.Logging;
@@ -16,80 +15,6 @@ namespace CompetitiveMatch;
 
 public partial class CompetitiveMatch
 {
-    public void OnChangeMatchBotFill(object? sender, bool value)
-    {
-        var quota = value ? match_max_players.Value : 0;
-        ExecuteCommands([
-            "bot_quota_mode fill",
-            $"bot_quota {quota}"
-        ]);
-    }
-
-    public void OnMapStart(string mapname)
-    {
-        IsInitialized = false;
-    }
-
-    public void OnTick()
-    {
-        if (!IsInitialized)
-        {
-            StartWarmup();
-            IsInitialized = true;
-        }
-
-        if (Match.Phase == MatchPhase_t.Warmup ||
-            match_bot_fill.Value)
-        {
-            var players = Utilities.GetPlayers();
-
-            if (Match.Phase == MatchPhase_t.Warmup)
-            {
-                foreach (var player in players)
-                {
-                    if (!player.IsBot)
-                    {
-                        var isReady = IsPlayerInATeam(player) && GetPlayerState(player)?.IsReady == true;
-                        SetPlayerClan(player, Localizer[isReady ? "match.ready" : "match.not_ready"]);
-                    }
-                }
-            }
-
-            if (match_bot_fill.Value)
-            {
-                var maxPlayers = match_max_players.Value / 2;
-                foreach (var team in MatchTeams)
-                {
-                    int botCount = 0;
-                    int humanCount = 0;
-                    int? botToKick = null;
-                    foreach (var player in players)
-                    {
-                        if (player.Team == team)
-                        {
-                            if (player.IsBot)
-                            {
-                                botCount++;
-                                if (botToKick == null && player.UserId != null)
-                                {
-                                    botToKick = player.UserId;
-                                }
-                            }
-                            else
-                            {
-                                humanCount++;
-                            }
-                        }
-                    }
-                    if (botCount + humanCount > maxPlayers && botToKick != null)
-                    {
-                        Server.ExecuteCommand($"kickid {botToKick}");
-                    }
-                }
-            }
-        }
-    }
-
     public HookResult OnPlayerConnectFull(EventPlayerConnectFull @event, GameEventInfo _)
     {
         var player = @event.Userid;
---
+++ source/CompetitiveMatch/CompetitiveMatch.Listeners.cs
@@ -0,0 +1,85 @@
+﻿/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Ian Lucas. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using CounterStrikeSharp.API;
+
+namespace CompetitiveMatch;
+
+public partial class CompetitiveMatch
+{
+    public void OnChangeMatchBotFill(object? sender, bool value)
+    {
+        var quota = value ? match_max_players.Value : 0;
+        ExecuteCommands([
+            "bot_quota_mode fill",
+            $"bot_quota {quota}"
+        ]);
+    }
+
+    public void OnMapStart(string mapname)
+    {
+        IsInitialized = false;
+    }
+
+    public void OnTick()
+    {
+        if (!IsInitialized)
+        {
+            StartWarmup();
+            IsInitialized = true;
+        }
+
+        if (Match.Phase == MatchPhase_t.Warmup ||
+            match_bot_fill.Value)
+        {
+            var players = Utilities.GetPlayers();
+
+            if (Match.Phase == MatchPhase_t.Warmup)
+            {
+                foreach (var player in players)
+                {
+                    if (!player.IsBot)
+                    {
+                        var isReady = IsPlayerInATeam(player) && GetPlayerState(player)?.IsReady == true;
+                        SetPlayerClan(player, Localizer[isReady ? "match.ready" : "match.not_ready"]);
+                    }
+                }
+            }
+
+            if (match_bot_fill.Value)
+            {
+                var maxPlayers = match_max_players.Value / 2;
+                foreach (var team in MatchTeams)
+                {
+                    int botCount = 0;
+                    int humanCount = 0;
+                    int? botToKick = null;
+                    foreach (var player in players)
+                    {
+                        if (player.Team == team)
+                        {
+                            if (player.IsBot)
+                            {
+                                botCount++;
+                                if (botToKick == null && player.UserId != null)
+                                {
+                                    botToKick = player.UserId;
+                                }
+                            }
+                            else
+                            {
+                                humanCount++;
+                            }
+                        }
+                    }
+                    if (botCount + humanCount > maxPlayers && botToKick != null)
+                    {
+                        Server.ExecuteCommand($"kickid {botToKick}");
+                    }
+                }
+            }
+        }
+    }
+}
